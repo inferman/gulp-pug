e4a63014a7d32e309e6ed49db8315483e2269d30
InferMan <inferman.manowar@gmail.com>
2017-08-14 10:20:19 +0300

    installed pug

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,6 +5,8 @@ import yargs         from 'yargs';
 import browser       from 'browser-sync';
 import gulp          from 'gulp';
 import panini        from 'panini';
+import jade	         from 'gulp-jade';
+import pug	         from 'gulp-pug';
 import rimraf        from 'rimraf';
 import sherpa        from 'style-sherpa';
 import yaml          from 'js-yaml';
@@ -49,14 +51,12 @@ function copy() {
 }
 
 // Copy page templates into finished HTML files
+
+
 function pages() {
-  return gulp.src('src/pages/**/*.{html,hbs,handlebars}')
-    .pipe(panini({
-      root: 'src/pages/',
-      layouts: 'src/layouts/',
-      partials: 'src/partials/',
-      data: 'src/data/',
-      helpers: 'src/helpers/'
+  return gulp.src('src/pages/*.pug')
+    .pipe(pug({
+      client: false,
     }))
     .pipe(gulp.dest(PATHS.dist));
 }
@@ -150,8 +150,10 @@ function reload(done) {
 // Watch for changes to static assets, pages, Sass, and JavaScript
 function watch() {
   gulp.watch(PATHS.assets, copy);
-  gulp.watch('src/pages/**/*.html').on('all', gulp.series(pages, browser.reload));
-  gulp.watch('src/{layouts,partials}/**/*.html').on('all', gulp.series(resetPages, pages, browser.reload));
+  //gulp.watch('src/pages/**/*.html').on('all', gulp.series(pages, browser.reload));
+  //gulp.watch('src/{layouts,partials}/**/*.html').on('all', gulp.series(resetPages, pages, browser.reload));
+  gulp.watch('src/pages/**/*.pug').on('all', gulp.series(pages, browser.reload));
+  gulp.watch('src/layouts/**/*.pug').on('all', gulp.series(resetPages, pages, browser.reload));
   gulp.watch('src/assets/scss/**/*.scss').on('all', sass);
   gulp.watch('src/assets/js/**/*.js').on('all', gulp.series(javascript, browser.reload));
   gulp.watch('src/assets/img/**/*').on('all', gulp.series(images, browser.reload));
